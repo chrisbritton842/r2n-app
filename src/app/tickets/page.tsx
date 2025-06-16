19498b3f0591a57c1840a5b733a994bc8bf6612c
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-06-15 23:21:06 -0700

    added utility classes to tickets page

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -10,17 +10,26 @@ const TICKET_ICONS = {
 
 const TicketsPage = () => {
     return (
-        <div>
-            {initialTickets.map((ticket) => (
-                <div key={ticket.id}>
-                    <div>{TICKET_ICONS[ticket.status]}</div>
-                    <h2 className="text-lg">{ticket.title}</h2>
+        <div className="flex-1 flex flex-col gap-y-8">
+            <div>
+                <h2 className="text-3x1 font-bold tracking-tight">Tickets</h2>
+                <p className="text-sm text-muted-foreground">
+                    All your tickets at one place
+                </p>
+            </div>
+            <div className="flex-1 flex flex-col items-center gap-y-4">
+                {initialTickets.map((ticket) => (
+                    <div key={ticket.id} className="w-full max-w-[420px]">
+                        <div>{TICKET_ICONS[ticket.status]}</div>
+                        <h3 className="text-lg">{ticket.title}</h3>
+                        <p className="text-sm">{ticket.content}</p>
 
-                    <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                      View
-                    </Link>
-                </div>
-            ))}
+                        <Link href={ticketPath(ticket.id)} className="text-sm underline">
+                            View
+                        </Link>
+                    </div>
+                ))}
+            </div>  
         </div>
     );
 };
