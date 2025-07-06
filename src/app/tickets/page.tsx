0f853fe42cca28a0b778b50ad3fb557a7ba3bbea
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-07-05 17:57:50 -0700

    added isDetail prop to TicketItem component to conditionally render card detail

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,94 +1,17 @@
-import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react";
-import Link from "next/link";
 import { Heading } from "@/components/heading";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { initialTickets } from "@/data";
-import { ticketPath } from "@/paths";
-
-// const CheckIcon = () => (
-//     <svg
-//       xmlns="http://www.w3.org/2000/svg"
-//       fill="none"
-//       viewBox="0 0 24 24"
-//       strokeWidth={1.5}
-//       stroke="currentColor"
-//       className="w-6 h-6"
-//     >
-//       <path
-//         strokeLinecap="round"
-//         strokeLinejoin="round"
-//         d="m4.5 12.75 6 6 9-13.5"
-//       />
-//     </svg>
-// );
-
-// const DocumentIcon = () => (
-//     <svg
-//       xmlns="http://www.w3.org/2000/svg"
-//       fill="none"
-//       viewBox="0 0 24 24"
-//       strokeWidth={1.5}
-//       stroke="currentColor"
-//       className="size-6"
-//     >
-//       <path
-//         strokeLinecap="round"
-//         strokeLinejoin="round"
-//         d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-//       />
-//     </svg>
-// );
-
-// const PencilIcon = () => (
-//     <svg
-//       xmlns="http://www.w3.org/2000/svg"
-//       fill="none"
-//       viewBox="0 0 24 24"
-//       strokeWidth={1.5}
-//       stroke="currentColor"
-//       className="size-6"
-//     >
-//       <path
-//         strokeLinecap="round"
-//         strokeLinejoin="round"
-//         d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-//       />
-//     </svg>
-// );
-
-const TICKET_ICONS = {
-    OPEN: <LucideFileText />,
-    IN_PROGRESS: <LucidePencil />,
-    DONE: <LucideCheckCircle />,
-};
+import { TicketItem } from "@/features/ticket/components/ticket-item";
 
 const TicketsPage = () => {
     return (
-        <div className="flex-1 flex flex-col gap-y-8">
-            <Heading title="Tickets" description="All your tickets at one place" />
-            <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-                {initialTickets.map((ticket) => (
-                    <Card key={ticket.id} className="w-full max-w-[420px]">
-                      <CardHeader>
-                        <CardTitle className="flex gap-x-2">
-                          <span>{TICKET_ICONS[ticket.status]}</span>
-                          <span className="truncate">{ticket.title}</span>
-                        </CardTitle>
-                      </CardHeader>
-                      <CardContent>
-                        <span className="line-clamp-3 whitespace-break-spaces">
-                          {ticket.content}
-                        </span>
-                      </CardContent>
-                      <CardFooter>
-                        <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                            View
-                        </Link>
-                      </CardFooter>
-                    </Card>
-                ))}
-            </div>  
+      <div className="flex-1 flex flex-col gap-y-8">
+        <Heading title="Tickets" description="All your tickets at one place" />
+        <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
+          {initialTickets.map((ticket) => (
+            <TicketItem key={ticket.id} ticket={ticket} />
+          ))}
         </div>
+      </div>
     );
 };
 
