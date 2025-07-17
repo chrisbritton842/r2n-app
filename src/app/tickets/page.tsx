a1292fa460b58c25794f0a29cef2b7969003f7a4
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-07-16 22:27:34 -0700

    remove async keyword from TicketsPage function signature

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import { Heading } from "@/components/heading";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
-const TicketsPage = async () => {
+const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
