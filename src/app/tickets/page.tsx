7242a204f5eb34998dfec6e3fb7f316facb05941
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-08-01 23:48:14 -0700

    revalidate tickets path

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -5,7 +5,7 @@ import { Placeholder } from "@/components/placeholder";
 import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
-const TicketsPage = () => {
+const TicketsPage = async () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
