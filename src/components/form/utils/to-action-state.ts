bc84ee111f443351cab1dddfcae75c44891b9c34
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-08-28 13:00:05 -0700

    create action state utils

--- src/components/form/utils/to-action-state.ts
+++ src/components/form/utils/to-action-state.ts
@@ -1,6 +1,30 @@
 import { ZodError } from "zod";
 
-export type ActionState = { message: string, payload?: FormData };
+export const EMPTY_ACTION_STATE: ActionState = {
+    message: "",
+    fieldErrors: {},
+    timestamp: Date.now(),
+};
+
+export type ActionState = {
+    status?: "SUCCESS" | "ERROR";
+    message: string;
+    payload?: FormData;
+    fieldErrors: Record<string, string[] | undefined>;
+    timestamp: number;
+};
+
+export const toActionState = (
+    status: ActionState["status"],
+    message: string
+): ActionState => {
+    return {
+        status,
+        message,
+        fieldErrors: {},
+        timestamp: Date.now(),
+    };
+};
 
 export const fromErrorToActionState = (
     error: unknown,
@@ -9,22 +33,31 @@ export const fromErrorToActionState = (
     if (error instanceof ZodError) {
         // if validation error with Zod, return first error message
         return {
-            message: error.issues[0].message,
+            status: "ERROR",
+            message: "",
             payload: formData,
+            fieldErrors: error.flatten().fieldErrors,
+            timestamp: Date.now(),
         };
     } else if (error instanceof Error) {
         // if another error instance, return error message
         // e.g. database error
         return {
+            status: "ERROR",
             message: error.message,
             payload: formData,
+            fieldErrors: {},
+            timestamp: Date.now(),
         };
     } else {
         // if not an error instance but something else crashed
         // return generic error message
         return {
+            status: "ERROR",
             message: "An unknown error occurred",
             payload: formData,
+            fieldErrors: {},
+            timestamp: Date.now(),
         };
     }
 };
