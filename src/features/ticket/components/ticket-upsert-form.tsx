161db25c63a68babfaf369bf65f166690eb5f0f6
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-09-02 11:41:32 -0700

    add custom hook

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
 import { FieldError } from "@/components/form/field-error";
+import { useActionFeedback } from "@/components/form/hooks/use-action-feedback";
 import { SubmitButton } from "@/components/form/submit-button";
 import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
 import { Input } from "@/components/ui/input";
@@ -19,6 +20,17 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
         upsertTicket.bind(null, ticket?.id),
         EMPTY_ACTION_STATE
     );
+
+    useActionFeedback(actionState, {
+        onSuccess: ({ actionState }) => {
+            console.log(actionState.message);
+            // TODO optionally handle success
+        },
+        onError: ({ actionState }) => {
+            console.log(actionState.message);
+            // TODO optionally handle error
+        },
+    });
 
     return (
         <form
@@ -47,8 +59,6 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
             <FieldError actionState={actionState} name="content" />
 
             <SubmitButton label={ticket ? "Edit" : "Create"} />
-
-            {actionState.message}
         </form>
     );
 };
