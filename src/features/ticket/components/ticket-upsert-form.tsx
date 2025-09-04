b7d0b658952cfa189a3b1363c9adccca3334d57e
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-09-04 10:32:37 -0700

    abstract new Form component

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import { useActionState } from "react";
-import { toast } from "sonner";
 import { FieldError } from "@/components/form/field-error";
-import { useActionFeedback } from "@/components/form/hooks/use-action-feedback";
+import { Form } from "@/components/form/form"
 import { SubmitButton } from "@/components/form/submit-button";
 import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
 import { Input } from "@/components/ui/input";
@@ -22,23 +21,10 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
         EMPTY_ACTION_STATE
     );
 
-    useActionFeedback(actionState, {
-        onSuccess: ({ actionState }) => {
-            if (actionState.message) {
-                toast.success(actionState.message);
-            }
-        },
-        onError: ({ actionState }) => {
-            if (actionState.message) {
-                toast.error(actionState.message);
-            }
-        },
-    });
-
     return (
-        <form
+        <Form
             action={action}
-            className="flex flex-col gap-y-2"
+            actionState={actionState}
         >
             <Label htmlFor="title">Title</Label>
             <Input
@@ -62,7 +48,7 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
             <FieldError actionState={actionState} name="content" />
 
             <SubmitButton label={ticket ? "Edit" : "Create"} />
-        </form>
+        </Form>
     );
 };
 
