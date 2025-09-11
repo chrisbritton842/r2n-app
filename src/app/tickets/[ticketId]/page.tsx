b6f7bbb9e99ffb4082d85744cec6b866bd799eef
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-09-11 11:53:21 -0700

    add RedirectToast component to layout

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,5 +1,4 @@
 import { notFound } from "next/navigation";
-import { RedirectToast } from "@/components/redirect-toast";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
 
@@ -23,8 +22,6 @@ const TicketPage = async ({ params }: TicketPageProps) => {
         <div className="flex justify-center animate-fade-from-top">
           <TicketItem ticket={ticket} isDetail />
         </div>
-
-        <RedirectToast />
       </>
     );
 };
