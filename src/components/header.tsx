c4c459f493be3b57f543e6f39295e829b098db9e
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-11-18 12:13:32 -0800

    dynamically render navigation items based on user authentication status

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,15 @@
 import { LucideKanban, LucideLogOut } from "lucide-react";
 import Link from "next/link";
 import { signOut } from "@/features/auth/actions/sign-out";
+import { getAuth } from "@/features/auth/queries/get-auth";
 import { homePath, signInPath, signUpPath, ticketsPath } from "@/paths";
 import { SubmitButton } from "./form/submit-button";
 import { ThemeSwitcher } from "./theme/theme-switcher";
 import { buttonVariants } from "./ui/button";
 
-const Header = () => {
-    const navItems = (
+const Header = async () => {
+    const { user } = await getAuth();
+    const navItems = user ? (
         <>
             <Link
                 href={ticketsPath()}
@@ -15,6 +17,12 @@ const Header = () => {
             >
                 Tickets
             </Link>
+            <form action={signOut}>
+                <SubmitButton label="Sign Out" icon={<LucideLogOut />} />
+            </form>
+        </>
+    ) : (
+        <>
             <Link
                 href={signUpPath()}
                 className={buttonVariants({ variant: "outline"})}
@@ -27,9 +35,6 @@ const Header = () => {
             >
                 Sign In
             </Link>
-            <form action={signOut}>
-                <SubmitButton label="Sign Out" icon={<LucideLogOut />} />
-            </form>
         </>
     );
 
