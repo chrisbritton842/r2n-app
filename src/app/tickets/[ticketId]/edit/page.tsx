9db95efda62b80668e66da84de43131ace0a903f
Chris Britton <chrisbritton@Chriss-Laptop.local>
2025-12-24 14:59:12 -0800

    check authorization

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -1,5 +1,7 @@
 import { notFound } from "next/navigation";
 import { CardCompact } from "@/components/card-compact";
+import { getAuth } from "@/features/auth/queries/get-auth";
+import { isOwner } from "@/features/auth/utils/is-owner";
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
 
@@ -10,8 +12,16 @@ type TicketEditPageProps = {
 };
 
 const TicketEditPage = async ({ params }: TicketEditPageProps) => {
+    const { user } = await getAuth();
     const { ticketId } = await params;
     const ticket = await getTicket(ticketId);
+
+    const isTicketFound = !!ticket;
+    const isTicketOwner = isOwner(user, ticket);
+    
+    if (!isTicketFound || !isTicketOwner) {
+        notFound();
+    }
 
     if (!ticket) {
         notFound();
