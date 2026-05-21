35adf9343b9f02c7c09af3cff418bcc390239be5
Chris Britton <chrisbritton@Chriss-Laptop.local>
2026-05-21 14:07:51 -0700

    create tabs component

--- src/app/account/password/page.tsx
+++ src/app/account/password/page.tsx
@@ -1,16 +1,16 @@
 import { Heading } from "@/components/heading";
 import { AccountTabs } from "@/features/account/components/account-tabs";
 
-const ProfilePage = () => {
+const PasswordPage = () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
             <Heading
-                title="Profile"
-                description="All your profile information"
+                title="Password"
+                description="Keep your account secure"
                 tabs={<AccountTabs />}
             />
         </div>
     );
 };
 
-export default ProfilePage;
+export default PasswordPage;
